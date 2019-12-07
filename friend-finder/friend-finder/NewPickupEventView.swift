7efd501806fc1c30862c7dfea4e68cadddfffabe
David <davidguido@litlabproductions.com>
2019-12-07 05:00:21 -0800

    Removed old files

--- friend-finder/friend-finder/NewPickupEventView.swift
+++ friend-finder/friend-finder/NewPickupEventView.swift
@@ -1,83 +1,85 @@
-//
-//  NewPickupEventView.swift
-//  friend-finder
-//
-//  Created by David Guido on 12/4/19.
-//  Copyright © 2019 CSUF. All rights reserved.
-//
+    //
+    //  NewPickupEventView.swift
+    //  friend-finder
+    //
+    //  Created by David Guido on 12/4/19.
+    //  Copyright © 2019 CSUF. All rights reserved.
+    //
 
-import SwiftUI
+    import SwiftUI
 
-struct NewPickupEventView: View
-{
-             // Pick Up Event
-             @Binding var cwid: String
-        
-             // Event
-             @Binding var eventName: String
-             @Binding var eventDay: Int
-             @Binding var eventMonth: Int
-             @Binding var eventTime: Int      // (24 hr) = (1440 min)
-    
-    
-    
-        @ObservedObject var session = FirebaseSession()
-        
-        @Environment(\.presentationMode) var presentationMode
+    struct NewPickupEventView: View {
+                 // Pick Up Event
+                @State var cwid: String
+            
+                 // Event
+              //   var eventName: String
+              //   var eventDay: Int
+              //   var eventMonth: Int
+               //  var eventTime: Int      // (24 hr) = (1440 min)
         
         
-        var body: some View {
-            Group {
-                VStack {
-                    HStack {
-                        TextField("Enter your campus ID", text: $cwid)
-                            .padding()
-                    }
-                    HStack {
-                        TextField("Enter event name", text: $eventName)
-                            .padding()
-                    }
-                    HStack {
-                        TextField("Enter event day", number: $eventDay)
-                            .padding()
-                    }
-                    HStack {
-                        TextField("Enter event month", text: $eventMonth)
-                            .padding()
-                    }
-                    HStack {
-                        TextField("Enter event time", text: $eventTime)
-                            .padding()
-                    }
-                    Button(action: {
-                        self.addPickupEvent()
-                    }) {
-                        Text("Add")
+
+            @ObservedObject var session = FirebaseSession()
+            
+            @Environment(\.presentationMode) var presentationMode
+            
+            
+            var body: some View {
+                Group {
+                    VStack {
+                      
+                        HStack {
+                            TextField("Enter your campus ID", text: $cwid)
+                                .padding()
+                        }
+                        
+    /*                    HStack {
+                            TextField("Enter event name", text: $eventName)
+                                .padding()
+                        }
+                        HStack {
+                            TextField("Enter event day",text: $eventDay)
+                                .padding()
+                        }
+                        HStack {
+                            TextField("Enter event month", text: $eventMonth)
+                                .padding()
+                        }
+                        HStack {
+                            TextField("Enter event time", text: $eventTime)
+                                .padding()
+                        }
+        */
+                        Button(action: {
+                            self.addPickupEvent()
+                        }) {
+                            Text("Add")
+                        }
                     }
                 }
+                .padding()
             }
-            .padding()
-        }
-        
-        func addPickupEvent() {
-           // if !newPUEvent.isEmpty {
-                
-                //Add PU Event to Firebase
-                
-                // func uploadPickUpEvent(id: String, name: String, day: Int, month: Int, time: Int)
+            
+            func addPickupEvent() {
+               // if !newPUEvent.isEmpty {
+                    
+                    //Add PU Event to Firebase
+                    
+                    // func uploadPickUpEvent(id: String, name: String, day: Int, month: Int, time: Int)
 
-            session.uploadPickUpEvent(id: cwid, name: eventName, day: eventDay, month: eventMonth, time: eventTime)
-                dismiss()
-         //   }
+                session.uploadPickUpEvent(id: "000000000", name: "name", day: 0, month: 0, time: 0)
+                    dismiss()
+             //   }
+            }
+            
+            func dismiss() {
+                presentationMode.wrappedValue.dismiss()
+            }
         }
-        
-        func dismiss() {
-            presentationMode.wrappedValue.dismiss()
+
+    struct NewPickupEventView_Previews: PreviewProvider {
+        static var previews: some View {
+            NewPickupEventView(cwid: "000000000")
         }
     }
-
-struct NewPickupEventView_Previews: PreviewProvider {
-    static var previews: some View {
-        NewPickupEventView()
-    }
-}
